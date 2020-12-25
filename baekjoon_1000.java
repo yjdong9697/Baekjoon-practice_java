6b3dd425b05685ee27651fedd8eb96742fd0785c
yjdong9697 <yjdong9697@yonsei.ac.kr>
2020-12-26 00:06:34 +0900

    2020.12.26 ps

---
+++ baekjoon_1000.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class baekjoon_1000{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        in.close();
+        System.out.println(a + b);
+    }
+}
